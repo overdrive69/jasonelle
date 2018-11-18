102b5704f883a4234b244bcf94ac9054c067dcb8
Camilo <camilo@ninjas.cl>
2018-11-18 15:58:39 -0300

    improved deployment scripts

--- scripts/deploy/website.py
+++ scripts/deploy/website.py
@@ -4,7 +4,7 @@ import subprocess
 from os import environ
 
 # Deploys the website directory to the website repo
-subprocess.call(["git", "remote", "add", "website", "https://github.com/jasonelle/jasonelle.github.io"])
+subprocess.call(["git", "remote", "add", "website", "git@github.com:jasonelle/jasonelle.github.io.git"])
 subprocess.call(["git", "checkout", "-b", "develop"])
 subprocess.call(["rm", "-rf", "celljs"])
 subprocess.call(["rm", "-rf", "stjs"])
@@ -16,7 +16,9 @@ subprocess.call(["rm", "-rf", "LICENSE"])
 subprocess.call(["rm", "-rf", "celljs"])
 subprocess.call(["rm", "-rf", "scripts"])
 
-subprocess.call(["mv", "website/*", "."])
+subprocess.call(["cd", "website"])
+subprocess.call(["mv", "-R", "*", ".."])
+subprocess.call(["cd", ".."])
 subprocess.call(["rm", "-rf", "website"])
 subprocess.call(["git", "add", "."])
 
