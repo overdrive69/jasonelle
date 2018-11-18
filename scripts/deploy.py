87f8d92196964fa882bc6a8561a7fa0c0185102d
Camilo <camilo@ninjas.cl>
2018-11-18 15:52:27 -0300

    added deploy scripts

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,12 +2,17 @@
 # coding: utf-8
 import subprocess
 
+subprocess.call(["git", "config", "--global" , "user.email" "travis@travis-ci.org"])
+subprocess.call(["git", "config", "--global" , "user.name" "Travis CI"])
+
 print("Deployment Script")
 
 print("Detecting Changes")
 
 print("Deploying to Repos")
 
-subprocess.call(["python", "scripts/deploy/website.py"])
+print("Deploying Website")
+result = subprocess.check_output(["python", "scripts/deploy/website.py"]).decode()
+print("Result", result)
 
 print("Jobs Done")
--- scripts/deploy/website.py
+++ scripts/deploy/website.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python3
+# coding: utf-8
+import subprocess
+from os import environ
 
 # Deploys the website directory to the website repo
+subprocess.call(["git", "remote", "add", "website", "https://github.com/jasonelle/jasonelle.github.io"])
+subprocess.call(["git", "checkout", "-b", "develop"])
+subprocess.call(["rm", "-rf", "celljs"])
+subprocess.call(["rm", "-rf", "stjs"])
+subprocess.call(["rm", "-rf", "jasonette"])
+subprocess.call(["rm", "-rf", ".gitignore"])
+subprocess.call(["rm", "-rf", ".travis.yml"])
+subprocess.call(["rm", "-rf", "README.md"])
+subprocess.call(["rm", "-rf", "LICENSE"])
+subprocess.call(["rm", "-rf", "celljs"])
+subprocess.call(["rm", "-rf", "scripts"])
+
+subprocess.call(["mv", "website/*", "."])
+subprocess.call(["rm", "-rf", "website"])
+subprocess.call(["git", "add", "."])
+
+message =  "Travis build: " + environ["TRAVIS_BUILD_NUMBER"]
+subprocess.call(["git", "commit", "-m", message])
+subprocess.call(["git", "push", "website", "develop"])
+subprocess.call(["git", "checkout", "master"])
+subprocess.call(["git", "branch", "-D", "develop"])
+
 print("Deployed Website")
