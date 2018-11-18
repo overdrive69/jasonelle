caebabffdb877af6871ee253ce48064fb86d465c
Camilo <camilo@ninjas.cl>
2018-11-18 19:25:12 -0300

    changed to https

--- scripts/cascades/website.py
+++ scripts/cascades/website.py
@@ -11,7 +11,7 @@ try:
     subprocess.check_call(
         [
             "git", "remote", "add", "website", 
-            "git@github.com:jasonelle/jasonelle.github.io.git"
+            "https://github.com/jasonelle/jasonelle.github.io.git"
         ]
     )
 except:
