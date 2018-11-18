cd6ed2eac2aa82bc373825de0467bf1a6c3a8345
Camilo <camilo@ninjas.cl>
2018-11-18 16:42:11 -0300

    improved deployment scripts

--- scripts/deploy/website.py
+++ scripts/deploy/website.py
@@ -20,8 +20,8 @@ subprocess.check_call(["rm", "-rf", "LICENSE"])
 subprocess.check_call(["rm", "-rf", "celljs"])
 subprocess.check_call(["rm", "-rf", "scripts"])
 
-wd = os.getcwd() + "/website"
-subprocess.check_call(["mv", "*", ".."], cwd=wd)
+wd = os.getcwd() + "website"
+print(subprocess.check_output(["mv", "*", ".."], cwd=wd))
 
 subprocess.check_call(["rm", "-rf", "website"])
 subprocess.check_call(["git", "add", "."])
